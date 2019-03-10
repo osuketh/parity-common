7031344cb6f5c0cfe2bcdbe16b9eef71cb511262
Osuke Sudo <dish230@gmail.com>
2019-03-10 12:00:54 +0900

    Chenge OsRng from ChaChaRng to be compatible with wasm

--- fixed-hash/src/hash.rs
+++ fixed-hash/src/hash.rs
@@ -513,7 +513,7 @@ macro_rules! impl_rand_for_fixed_hash {
 
 			/// Assign `self` to a cryptographically random value.
 			pub fn randomize(&mut self) {
-				let mut rng = $crate::rand::OsRng::new().unwrap();
+				let mut rng = $crate::rand::ChaChaRng::new_unseeded();
 				self.randomize_using(&mut rng);
 			}
 
